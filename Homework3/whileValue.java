7b37328ca55a2f01c27daa0badb0d9c7e1b76427
Ysaye <josias.ysaye@gmail.com>
2014-02-08 03:22:13 -0600

    Update whileValue.java

--- Homework3/whileValue.java
+++ Homework3/whileValue.java
@@ -7,15 +7,15 @@ Complete el algoritmo:
 Dado un numero mayor a cero, imprima el valor actual desde cero hasta antes de llegar dado valor.
 
 */
-import java.util.Scanner;
+
 
 public class whileValue {   
 
    public static void main(String[] args) {
-   	 Scanner teclado = new Scanner(System.in);// entrada de datos
+   	 int numero;
         
-        System.out.println("ingrese numero \n");//se pide en pantalla un numero
-        int numero= teclado.nextInt();
+        
+         numero= Integer.parseInt(args[0].toString());
         int contador = 0;
         
         while(contador<=numero){//se usa value para la condicion
